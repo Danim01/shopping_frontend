f4b329c00ab70349f973f62b0b0c142ba6b03938
Danim01 <danibuimon.09@gmail.com>
2024-11-23 02:00:38 -0500

    .

--- src/routes/private/Dashboard.jsx
+++ src/routes/private/Dashboard.jsx
@@ -4,11 +4,8 @@ import Card from "../../components/Card"
 
 
 export default function Dashboard() {
-  const { getProfile, user } = useAuth()
+  const { user } = useAuth()
 
-  useEffect (() => {
-    getProfile()
-  }, [getProfile])
   return (
     user ? (
       <Card name={user.name} lastName={user.last_name} correo={user.email}/>
